634bd3e978146f637e4bc3aae27e14705e176a66
vistritpandey <vistritpandey@gmail.com>
2020-10-26 15:57:40 +0530

    m

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Header'
+import Home from "./Home"
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Home />
     </div>
   );
 }
