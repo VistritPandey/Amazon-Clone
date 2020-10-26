ea46ce2e9c05c652698426b1ac0f6dc9098d6061
vistritpandey <vistritpandey@gmail.com>
2020-10-26 15:13:28 +0530

    m

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,14 @@ import './Header.css'
 function Header() {
     return (
         <div className="header">
-            <h1>I'm the header</h1>
+            <img className="header__logo" src="https://cdn.freebiesupply.com/logos/large/2x/amazon-dark-logo-png-transparent.png" />
+            <div className="header__search">
+                <input className="header__searchInput" type="text" />
+
+            </div>
+            <div className="header__nav">
+                
+            </div>
         </div>
     )
 }
