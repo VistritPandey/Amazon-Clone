fac3fbd40b4027c11963df335b8c418099a36f56
vistritpandey <vistritpandey@gmail.com>
2020-10-26 16:48:57 +0530

    m

--- src/Product.js
+++ src/Product.js
@@ -4,7 +4,16 @@ import "./Product.css"
 function Product() {
     return (
         <div className="product">
-            
+            <div className="product__info">
+                <p>Start</p>
+                <p className="product__price">
+                    <small>$</small>
+                    <strong>10.00</strong>
+                </p>
+                <div className="product__rating">
+                    <p>⭐⭐⭐⭐⭐</p>
+                </div>
+            </div>
         </div>
     )
 }
