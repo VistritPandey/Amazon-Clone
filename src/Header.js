f1acab864c680540f52982768429fb59c2e5b578
vistritpandey <vistritpandey@gmail.com>
2020-10-26 15:29:49 +0530

    m

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 function Header() {
     return (
         <div className="header">
-            <img className="header__logo" src="https://cdn.freebiesupply.com/logos/large/2x/amazon-dark-logo-png-transparent.png" />
+            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
             <div className="header__search">
                 <input className="header__searchInput" type="text" />
                 {/* Logo */}
@@ -20,11 +20,21 @@ function Header() {
                 </div>
 
                 <div className="header__option">
-                    
+                    <span className="header__optionLineOne">
+                        Return
+                    </span>
+                    <span className="header__optionLineTwo">
+                        & Orders
+                    </span>
                 </div>
 
                 <div className="header__option">
-                    
+                    <span className="header__optionLineOne">
+                        Your
+                    </span>
+                    <span className="header__optionLineTwo">
+                        Prime
+                    </span>
                 </div>
             </div>
         </div>
