7aa28e6615cc2b76e720a095b280aab1a29aaf67
vistritpandey <vistritpandey@gmail.com>
2020-10-26 16:36:46 +0530

    m

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,10 @@ import './Home.css'
 function Home() {
     return (
         <div className="home">
-            
+            <div className="home__container">
+                <img className="home__image" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/October/Fuji_Tallhero_Dash_en_US_1x._CB418727898_.jpg" alt="" />
+                
+            </div>
         </div>
     )
 }
