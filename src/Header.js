0007ccc1edbec3cc7c1c5d3a6beb57464a6b157c
vistritpandey <vistritpandey@gmail.com>
2020-10-26 15:20:24 +0530

    m

--- src/Header.js
+++ src/Header.js
@@ -7,10 +7,25 @@ function Header() {
             <img className="header__logo" src="https://cdn.freebiesupply.com/logos/large/2x/amazon-dark-logo-png-transparent.png" />
             <div className="header__search">
                 <input className="header__searchInput" type="text" />
-
+                {/* Logo */}
             </div>
             <div className="header__nav">
-                
+                <div className="header__option">
+                    <span className="header__optionLineOne">
+                        Hello Customer
+                    </span>
+                    <span className="header__optionLineTwo">
+                        Sign In
+                    </span>
+                </div>
+
+                <div className="header__option">
+                    
+                </div>
+
+                <div className="header__option">
+                    
+                </div>
             </div>
         </div>
     )
