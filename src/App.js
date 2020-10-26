0f0b28a86c8ce10cd88c02e4d4119e4a4e1bf21a
vistritpandey <vistritpandey@gmail.com>
2020-10-26 15:04:52 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './Header'
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello</p>
+    <div className="app">
+      <Header />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>I'm the header</h1>
+        </div>
+    )
+}
+
+export default Header
