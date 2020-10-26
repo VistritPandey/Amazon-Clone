77079307da52d216d4cf7fa47bef3d2b09b337dc
vistritpandey <vistritpandey@gmail.com>
2020-10-26 16:41:58 +0530

    m

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Home.css'
+import Product from './Product'
 
 function Home() {
     return (
@@ -7,6 +8,22 @@ function Home() {
             <div className="home__container">
                 <img className="home__image" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/October/Fuji_Tallhero_Dash_en_US_1x._CB418727898_.jpg" alt="" />
                 
+                <div className="home__row">
+                    <Product />
+                    <Product />
+                </div>
+
+                <div className="home__row">
+                    <Product />
+                    <Product />
+                    <Product />
+
+                </div>
+
+                <div className="home__row">
+                    <Product />
+                    <Product />
+                </div>
             </div>
         </div>
     )
---
+++ src/Product.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Product.css"
+
+function Product() {
+    return (
+        <div className="product">
+            
+        </div>
+    )
+}
+
+
+export default Product
