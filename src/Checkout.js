ecc5c26c2ff2ce417000e9132e89d16cc174b6e7
vistritpandey <vistritpandey@gmail.com>
2020-10-27 15:18:47 +0530

    F

--- src/Checkout.js
+++ src/Checkout.js
@@ -6,6 +6,13 @@ function Checkout() {
         <div className="checkout">
             <div className="checkout__left">
                 <img className="checkout__ad" src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/jupiter20/GWphase2/v1/Phase2_Rec_PC_Hero_1x._CB417639218_.jpg" alt=""/>
+                <div>
+                    <h2 className="checkout__title"> Your Shopping Cart</h2>
+                        {/*Basket Item*/}
+                </div>
+            </div>
+            <div className="checkout__right">
+                <h2>Your subtotal</h2>
             </div>
         </div>
     )
