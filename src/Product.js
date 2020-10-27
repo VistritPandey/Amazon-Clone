7b42687ad206071a1737603824049180b7a98c8f
vistritpandey <vistritpandey@gmail.com>
2020-10-27 12:04:44 +0530

    m

--- src/Product.js
+++ src/Product.js
@@ -5,7 +5,7 @@ function Product() {
     return (
         <div className="product">
             <div className="product__info">
-                <p>OnrPlus 8t</p>
+                <p>OnePlus 8t</p>
                 <p className="product__price">
                     <small>$</small>
                     <strong>10.00</strong>
@@ -15,7 +15,7 @@ function Product() {
                 </div>
             </div>
             <img src="https://images-eu.ssl-images-amazon.com/images/I/41ppKgnVLdL._AC_US160_FMwebp_QL70_.jpg" alt="" />
-            <button>Add to cart</button>
+            <button className="product__button">Add to cart</button>
         </div>
     )
 }
