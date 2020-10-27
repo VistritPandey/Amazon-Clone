0139df82b123f3d6a510f6804d14f645f46f08c0
vistritpandey <vistritpandey@gmail.com>
2020-10-27 17:33:10 +0530

    m

--- src/Product.js
+++ src/Product.js
@@ -6,6 +6,16 @@ function Product({id, title, image, price, rating }) {
     const [{ basket }, dispatch] = useStateValue();
 
     const addToBasket = () => {
+        dispatch({
+            type:"ADD_TO_BASKET",
+            item:{
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating,
+            },
+        });
 
     }
     return (
