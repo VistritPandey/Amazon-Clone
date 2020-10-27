3d7162821ce5bc08d8fe9872241a832e8a949179
vistritpandey <vistritpandey@gmail.com>
2020-10-27 14:59:20 +0530

    m

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import './App.css';
 import Header from './Header'
 import Home from "./Home"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Checkout from "./Checkout"
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+      <Switch >
+        <Route path="/checkout">
+          <Header />
+          <Checkout />
+        </Route>
+        <Route path="/">
+          <Header />
+          <Home />
+        </Route>
+      </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Checkout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Checkout.css"
+
+function Checkout() {
+    return (
+        <div>
+            <h1>I am the Checkout</h1>
+        </div>
+    )
+}
+
+export default Checkout
