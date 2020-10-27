1d42425304d9395082444ec46f78580e4e17c00d
vistritpandey <vistritpandey@gmail.com>
2020-10-27 14:44:54 +0530

    react router dom added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './Header'
 import Home from "./Home"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
   return (
