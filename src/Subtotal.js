876dcd5e32ea8f4410fd5c70a14400751215bcb5
Vistrit Pandey <vistritpandey@gmail.com>
2020-10-28 16:55:58 +0530

    m

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "./Subtotal.css"
 import CurrencyFormat from "react-currency-format"
 import { useStateValue } from "./StateProvider"
+import { getBasketTotal } from './reducer';
 
 function Subtotal() {
     const [{ basket }, dispatch] = useStateValue();
@@ -20,7 +21,7 @@ function Subtotal() {
                     </>
                 )}
                 decimalScale={2}
-                value={0}
+                value={getBasketTotal(basket)}
                 displayType={"text"}
                 thousandSeperator={true}
                 prefix={"₹"}
