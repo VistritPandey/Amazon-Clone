20a1a55c5b2b808c4b2b79b254f3762360a08e46
Vistrit Pandey <vistritpandey@gmail.com>
2020-10-28 17:02:28 +0530

    f

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -13,7 +13,7 @@ function Subtotal() {
                 renderText={(value) => (
                     <>
                         <p>
-                            Subtotal ({basket.length} items) : <strong>0</strong>
+                Subtotal ({basket.length} items) : <strong>{value}</strong>
                         </p>
                         <small className="subtotal__gift">
                             <input type="checkbox" /> This order contains a gift
