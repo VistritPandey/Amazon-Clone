54e88835977c275ece89916b5636d1e0fa78aaf4
Vistrit Pandey <vistritpandey@gmail.com>
2020-10-28 17:06:17 +0530

    m

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Checkout.css"
+import CheckoutProduct from './CheckoutProduct'
 import Subtotal from './Subtotal'
 
 function Checkout() {
@@ -9,7 +10,7 @@ function Checkout() {
                 <img className="checkout__ad" src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/jupiter20/GWphase2/v1/Phase2_Rec_PC_Hero_1x._CB417639218_.jpg" alt=""/>
                 <div>
                     <h2 className="checkout__title"> Your Shopping Cart</h2>
-                        {/*Basket Item*/}
+                        <CheckoutProduct />
                 </div>
             </div>
             <div className="checkout__right">
