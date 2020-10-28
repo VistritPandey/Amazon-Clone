3ca3b774c2a35f8ab704bff9e65d322f861482fe
Vistrit Pandey <vistritpandey@gmail.com>
2020-10-28 17:59:33 +0530

    done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './Header'
 import Home from "./Home"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Checkout from "./Checkout"
+import Login from './Login';
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
       <div className="app">
       <Header />
       <Switch >
+      <Route path="/login">
+          <Login />
+        </Route>
         <Route path="/checkout">
           <Checkout />
         </Route>
