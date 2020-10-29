7182e3aaa4f394cecd22f1d89b2b66adc0dc37a8
vistritpandey <vistritpandey@gmail.com>
2020-10-29 09:52:04 +0530

    m

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,12 +5,13 @@ import Subtotal from './Subtotal'
 import {useStateValue} from './StateProvider'
 
 function Checkout() {
-    const [{ basket }, dispatch] = useStateValue();
+    const [{ basket, user }, dispatch] = useStateValue();
     return (
         <div className="checkout">
             <div className="checkout__left">
                 <img className="checkout__ad" src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/jupiter20/GWphase2/v1/Phase2_Rec_PC_Hero_1x._CB417639218_.jpg" alt=""/>
                 <div>
+                    <h3>Hello, {user?.email}</h3>
                     <h2 className="checkout__title"> Your Shopping Cart</h2>
                         {basket.map(item =>(
                             <CheckoutProduct 
