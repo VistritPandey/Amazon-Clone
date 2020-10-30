a5083bb733b08544f2612b3cbfa2ecbbfe24f0a8
vistritpandey <vistritpandey@gmail.com>
2020-10-30 07:41:06 +0530

    complete

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -3,8 +3,10 @@ import "./Subtotal.css"
 import CurrencyFormat from "react-currency-format"
 import { useStateValue } from "./StateProvider"
 import { getBasketTotal } from './reducer';
+import { useHistory } from 'react-router-dom';
 
 function Subtotal() {
+    const history = useHistory();
     const [{ basket }, dispatch] = useStateValue();
 
     return (
@@ -26,7 +28,7 @@ function Subtotal() {
                 thousandSeperator={true}
                 prefix={"₹"}
             />
-            <button>Proceed to checkout</button>
+            <button onClick={e => history.push('/payment')}>Proceed to checkout</button>
         </div>
     )
 }
