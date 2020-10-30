e338616e5ef6f181348c87ab1a83807a9907f827
vistritpandey <vistritpandey@gmail.com>
2020-10-30 09:59:51 +0530

    fixed the bank side payment

--- src/Payment.js
+++ src/Payment.js
@@ -26,7 +26,7 @@ function Payment() {
         const getClientSecret = async () => {
             const response = await axios({
                 method: 'post',
-                url: `/payments/create?total=${getBasketTotal(basket)}`
+                url: `/payments/create?total=${getBasketTotal(basket) * 100}`
             });
             setClientSecret(response.data.clientSecret)
         }
