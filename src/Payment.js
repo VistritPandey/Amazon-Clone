9e244726c921901bc4f971f80088eee42e93756b
vistritpandey <vistritpandey@gmail.com>
2020-10-30 07:53:33 +0530

    m

--- src/Payment.js
+++ src/Payment.js
@@ -3,8 +3,79 @@ import './Payment.css'
 
 function Payment() {
     return (
-        <div>
-           <h1>I am the Payment</h1> 
+        <div className='payment'>
+            <div className='payment__container'>
+                <h1>
+                    Checkout (
+                        <Link to="/checkout">{basket?.length} items</Link>
+                        )
+                </h1>
+
+
+                {/* Payment section - delivery address */}
+                <div className='payment__section'>
+                    <div className='payment__title'>
+                        <h3>Delivery Address</h3>
+                    </div>
+                    <div className='payment__address'>
+                        <p>{user?.email}</p>
+                        <p>123 React Lane</p>
+                        <p>Los Angeles, CA</p>
+                    </div>
+                </div>
+
+                {/* Payment section - Review Items */}
+                <div className='payment__section'>
+                    <div className='payment__title'>
+                        <h3>Review items and delivery</h3>
+                    </div>
+                    <div className='payment__items'>
+                        {basket.map(item => (
+                            <CheckoutProduct
+                                id={item.id}
+                                title={item.title}
+                                image={item.image}
+                                price={item.price}
+                                rating={item.rating}
+                            />
+                        ))}
+                    </div>
+                </div>
+            
+
+                {/* Payment section - Payment method */}
+                <div className='payment__section'>
+                    <div className="payment__title">
+                        <h3>Payment Method</h3>
+                    </div>
+                    <div className="payment__details">
+                            {/* Stripe magic will go */}
+
+                            <form onSubmit={handleSubmit}>
+                                <CardElement onChange={handleChange}/>
+
+                                <div className='payment__priceContainer'>
+                                    <CurrencyFormat
+                                        renderText={(value) => (
+                                            <h3>Order Total: {value}</h3>
+                                        )}
+                                        decimalScale={2}
+                                        value={getBasketTotal(basket)}
+                                        displayType={"text"}
+                                        thousandSeparator={true}
+                                        prefix={"$"}
+                                    />
+                                    <button disabled={processing || disabled || succeeded}>
+                                        <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
+                                    </button>
+                                </div>
+
+                                  {/* Errors */}
+                                {error && <div>{error}</div>}
+                            </form>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
